498a3d01042a80cc6e847b8f5c360fbd2788f789
ChhaviPriyaTanwar <chhavipriyasing@gmail.com>
2025-04-03 12:10:27 +0530

    some chnages

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 const dbConfig = {
   host: process.env.DB_HOST || 'mysql',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'password',
+  password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'mydb',
   port: process.env.DB_PORT || 3306
 };
