51b39fb794927a3d3600ee9b8ea11ed916f22c85
Chhavi Priya Tanwar <160010758+ChhaviPriyaTanwar@users.noreply.github.com>
2025-04-04 10:02:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const dbConfig = {
   host: process.env.DB_HOST || '127.0.0.1',
   port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'root',
+  password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'mydb'
 };
 
